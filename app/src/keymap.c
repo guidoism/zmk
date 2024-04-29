f43579dac5619003e95d1807eb8a23564cc2c7f5
Guido Bartolucci <guido@bartolucci.org>
2024-04-29 17:07:12 -0400

    Layer change info for status script reading from console

--- app/src/keymap.c
+++ app/src/keymap.c
@@ -96,6 +96,7 @@ static inline int set_layer_state(uint8_t layer, bool state) {
     WRITE_BIT(_zmk_keymap_layer_state, layer, state);
     // Don't send state changes unless there was an actual change
     if (old_state != _zmk_keymap_layer_state) {
+        printk("GUIDO: layer %d, new state set: %d\n", layer, _zmk_keymap_layer_state);
         LOG_DBG("layer_changed: layer %d state %d", layer, state);
         ret = raise_layer_state_changed(layer, state);
         if (ret < 0) {
